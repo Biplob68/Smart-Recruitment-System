26e838516b6fd96418b77527057e1d820c54730f
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-04-18 12:36:55 +0600

    admin

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,11 @@ from . import views
 apps_name = 'mysite'
 
 urlpatterns = [
-          path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('about', views.about, name='about'),
+    path('jobsingle', views.jobsingle, name='jobsingle'),
+
+
 
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -4,4 +4,15 @@ from django.shortcuts import render
 def index(request):
     return render(request,'mysite/index.html')
 
+def login(request):
+    return render(request,'mysite/login.html')
+
+def about(request):
+    return render(request,'mysite/about.html')
+
+def jobsingle(request):
+    return render(request,'mysite/job-single.html')
+
+
+
 
