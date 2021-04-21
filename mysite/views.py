4ac1222591a5241cd78ab62103750314d56dd5c4
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-04-21 15:22:42 +0600

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def index(request):
-    return render(request,'mysite/index.html')
+    return render(request, 'mysite/index.html')
 
 def login(request):
-    return render(request,'mysite/login.html')
+    return render(request, 'mysite/login.html')
 
 def about(request):
-    return render(request,'mysite/about.html')
+    return render(request, 'mysite/about.html')
 
 def jobsingle(request):
-    return render(request,'mysite/job-single.html')
+    return render(request, 'mysite/job-single.html')
 
 
 
