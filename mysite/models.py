9cf4a83bccee7cf359f59d3ded85a898369925b0
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-05-02 01:28:13 +0600

    Update models.py

--- mysite/models.py
+++ mysite/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=111, default="")
