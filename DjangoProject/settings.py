f63f67bb5bfda1a23d8c5e243a3784d117247224
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-05-02 01:28:42 +0600

    Update settings.py

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'DjangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR,'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
