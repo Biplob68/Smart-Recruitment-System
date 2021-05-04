108ee36065cc2956854f886faed3b1037c0fed7b
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-05-05 02:41:35 +0600

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,14 +5,14 @@ from . import views
 #apps_name = 'mysite'
 
 urlpatterns = [
-    path("", views.index, name="home"),
+    path("", views.index, name="index"),
     path("login.html", views.login, name="login"),
-    path("register", views.register, name="register"),
-    path("logout", views.logout, name ="logout"),
-    path("about/", views.about, name="about"),
+    path("register.html", views.register, name="register"),
+    path("logout.html", views.logout, name ="logout"),
+    path("about.html", views.about, name="about"),
     path("job-single.html", views.jobsingle, name="jobsingle"),
-    path("post-job.html", views.postjob, name="jobsingle"),
-    path("contact/", views.contact, name="contact"),
+    path("post-job.html", views.postjob, name="postjob"),
+    path("contact.html", views.contact, name="contact"),
 
 
 ]
