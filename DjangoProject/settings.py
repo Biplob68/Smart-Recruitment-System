afb87d8c7b611db5ec494f200c2c8b0e678770c0
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-06-06 11:38:12 +0600

    Update settings.py

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
