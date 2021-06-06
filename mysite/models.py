f67f88a88cdcf5afb3d49e73528c19d026c94e90
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-06-06 11:38:52 +0600

    Update models.py

--- mysite/models.py
+++ mysite/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class contact(models.Model):
+class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=111, default="")
     phone = models.CharField(max_length=12, default="")
-    subject = models.CharField(max_length=100)
-    description = models.CharField(max_length=300)
+    subject = models.TextField(max_length=100)
+    description = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.name + " - " + self.email
