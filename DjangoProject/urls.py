cef324ac004b4c3f76bc58b24577e4553ba9a320
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-06-06 11:38:17 +0600

    Update urls.py

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mysite.urls')),
