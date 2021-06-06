ab7b3d246d360c3349314672b7e5854c5febf42c
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-06-06 11:39:40 +0600

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,18 +1,23 @@
-from django.urls import path
-
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
+
+# Django Admin header Customization
+admin.site.site_header = "Login for admin dashboard"
 #apps_name = 'mysite'
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name="index"),
     path("login.html", views.login, name="login"),
     path("register.html", views.register, name="register"),
     path("logout.html", views.logout, name ="logout"),
     path("about.html", views.about, name="about"),
-    path("job-single.html", views.jobsingle, name="jobsingle"),
+    path("job_listing.html", views.job_listing, name="job_listing"),
+    # path("job-single.html", views.jobsingle, name="jobsingle"),
     path("post-job.html", views.postjob, name="postjob"),
     path("contact.html", views.contact, name="contact"),
+
 
 
 ]
