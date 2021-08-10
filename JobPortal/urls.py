afb09b76ff53699f7a5b836f4e49610aa8915024
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-08-10 19:03:35 +0600

    update

--- JobPortal/urls.py
+++ JobPortal/urls.py
@@ -1,4 +1,4 @@
-"""DjangoProject URL Configuration
+"""JobPortal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -19,12 +19,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 from mysite.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('', include('mysite.urls')),
-
 ]
+
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
--- JobPortal/wsgi.py
+++ JobPortal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangoProject project.
+WSGI config for JobPortal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JobPortal.settings')
 
 application = get_wsgi_application()
